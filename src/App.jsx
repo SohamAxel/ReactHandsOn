c0faaa163f68a77c5bee48bd229028c81a653b72
Soham Sengupta <soham.sengupta@axelerant.com>
2024-09-18 10:16:17 +0530

    Improved folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
+import CounterName from "./Components/CounterName";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  return (
-    <>
-      <input type="text" name="name" id="name" value={name} onInput={(e) => setName(e.target.value)}/>
-      <div className="counter">
-        <button onClick={() => setAge(p => p - 1)}>-</button>
-        {age}
-        <button onClick={() => setAge(p => p + 1)}>+</button>
-      </div>
-      <p>{`My name is ${name} and I am ${age} years old`}</p>
-    </>
-  );
+  return <CounterName />;
 }
 
 export default App;
