f7448a05a89d9468e56330cbbcd1c17d59ccb2ea
Soham Sengupta <soham.sengupta@axelerant.com>
2024-09-19 10:42:11 +0530

    State form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,69 @@
-import './styles.css';
+import { useMemo, useState } from "react";
+import "./styles.css";
+import { validateEmail, validatePassword } from "./Validator/formValidator";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isAfterFirstSubmit, setIsAfterFirstSubmit] = useState(false);
+
+  const emailErrorList = useMemo(() => {
+    return isAfterFirstSubmit ? validateEmail(email) : [];
+  }, [email, isAfterFirstSubmit]);
+
+  const passwordErrorList = useMemo(() => {
+    return isAfterFirstSubmit ? validatePassword(password) : [];
+  }, [password, isAfterFirstSubmit]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsAfterFirstSubmit(true);
+
+    const checkEmail = validateEmail(email);
+    const checkPassword = validatePassword(password);
+
+    if (checkEmail.length === 0 && checkPassword.length === 0) {
+      alert("success");
+    }
+
+  };
+
   return (
-      
-    <form class="form">
-    <div class="form-group error">
-      <label class="label" for="email">Email</label>
-      <input class="input" type="email" id="email" value="test@test.com" />
-      <div class="msg">Must end in @webdevsimplified.com</div>
-    </div>
-    <div class="form-group">
-      <label class="label" for="password">Password</label>
-      <input
-        class="input"
-        value="Password123!"
-        type="password"
-        id="password"
-      />
-    </div>
-    <button class="btn" type="submit">Submit</button>
-  </form>
+    <form onSubmit={handleSubmit} className="form">
+      <div className={`form-group ${emailErrorList.length > 0 ? "error" : ""}`}>
+        <label className="label" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="input"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {
+          emailErrorList.length > 0 && <div className="msg">{emailErrorList.join(', ')}</div>
+        }
+      </div>
+      <div className={`form-group ${passwordErrorList.length > 0 ? "error" : ""}`}>
+        <label className="label" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="input"
+          value={password}
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {
+          passwordErrorList.length > 0 && <div className="msg">{passwordErrorList.join(', ')}</div>
+        }
+      </div>
+      <button className="btn" type="submit">
+        Submit
+      </button>
+    </form>
   );
 }
 
