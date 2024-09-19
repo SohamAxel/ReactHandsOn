7f7c8b436de9335ad3e62aa21cb60451d355a37b
Soham Sengupta <soham.sengupta@axelerant.com>
2024-09-19 09:20:05 +0530

    Initial before code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import './styles.css';
 
 function App() {
   return (
-      <h1>My App</h1>
+      
+    <form class="form">
+    <div class="form-group error">
+      <label class="label" for="email">Email</label>
+      <input class="input" type="email" id="email" value="test@test.com" />
+      <div class="msg">Must end in @webdevsimplified.com</div>
+    </div>
+    <div class="form-group">
+      <label class="label" for="password">Password</label>
+      <input
+        class="input"
+        value="Password123!"
+        type="password"
+        id="password"
+      />
+    </div>
+    <button class="btn" type="submit">Submit</button>
+  </form>
   );
 }
 
