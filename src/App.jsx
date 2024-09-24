a1d54c3396d4ac09bb92d256bbb1dc51c506b977
Soham Sengupta <soham.sengupta@axelerant.com>
2024-09-24 08:00:28 +0530

    Completed Modal task

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import CustomModal from "./Components/CustomModal";
+import DialogModal from "./Components/DialogModal";
+import "./styles.css";
 
 function App() {
   return (
+    <>
       <h1>My App</h1>
+      <CustomModal />
+      <DialogModal />
+    </>
   );
 }
 
---
+++ src/Components/DialogModal.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+
+const DialogModal = () => {
+  const [showDialogModal, setShowDialogModal] = useState(false);
+
+  const hideModal = () => {
+    setShowDialogModal(false);
+  };
+  const toggleModal = () => {
+    setShowDialogModal((p) => !p);
+  };
+
+  return (
+    <>
+      <button onClick={() => toggleModal()}>DialogModal</button>
+      <DialogModalPortal hideModal={hideModal} showDialogModal={showDialogModal}>
+        <p>
+          This is a <strong>CUSTOM</strong> modal
+        </p>
+      </DialogModalPortal>
+    </>
+  );
+};
+
+const DialogModalPortal = ({ hideModal, showDialogModal, children }) => {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    if (showDialogModal == false) {
+      dialogRef.current.close();
+    } else {
+      dialogRef.current.showModal();
+    }
+  }, [showDialogModal])
+
+  useEffect(() => {
+    if (showDialogModal == true) {
+      dialogRef.current.addEventListener('close', hideModal);
+    }
+
+    return () => {
+      document.body.removeEventListener('close', hideModal);
+    }
+  }, [hideModal]);
+
+  return createPortal(
+    <dialog ref={dialogRef}>
+      {children}
+      <button onClick={() => hideModal()}>Close</button>
+    </dialog>,
+    document.querySelector("#react-modals")
+  );
+};
+
+export default DialogModal;
