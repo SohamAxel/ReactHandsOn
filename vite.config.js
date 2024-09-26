b8880b023d37bfb2458866694da308a9b07374d3
Soham Sengupta <soham.sengupta@axelerant.com>
2024-09-26 12:18:53 +0530

    Completed Component testing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: false, // auto import if true
+    environment: "jsdom", // as we want to run test in dom and not in node.
+    setupFiles: "./setupTests.js", // Run all tests in this file, it can also accept array of file names
+  },
 })
