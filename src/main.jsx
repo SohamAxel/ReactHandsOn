7059599f6b5062c74e25e6207dc8a0553735bedf
Soham Sengupta <soham.sengupta@axelerant.com>
2024-10-04 08:40:42 +0530

    Route testing completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./styles.css"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { routes } from "./routes"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={createBrowserRouter(routes)} />
+  </React.StrictMode>
 )
