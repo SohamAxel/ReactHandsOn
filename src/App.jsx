e1053e2a1ffc58be8106a8bc16df7105677d0bfc
Soham Sengupta <soham.sengupta@axelerant.com>
2024-10-15 17:44:14 +0530

    Completed task

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,195 @@
-// useImperativeHandle: scroll to top/bottom
-// http://localhost:3000/isolated/exercise/05.js
 
 import * as React from "react";
-import "./styles.css";
-import { useImperativeHandle } from "react";
+import { dequal } from "dequal";
 
-// 🐨 wrap this in a React.forwardRef and accept `ref` as the second argument
-function InnerComponent({ messages }, ref) {
-  const containerRef = React.useRef();
-  React.useLayoutEffect(() => {
-    scrollToBottom();
-  });
 
-  // 💰 you're gonna want this as part of your imperative methods
-  function scrollToTop() {
-    containerRef.current.scrollTop = 0;
+import * as userClient from "./user-client";
+import { useAuth } from "./auth-context";
+
+const UserContext = React.createContext();
+UserContext.displayName = "UserContext";
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case "start update": {
+      return {
+        ...state,
+        user: { ...state.user, ...action.updates },
+        status: "pending",
+        storedUser: state.user,
+      };
+    }
+    case "finish update": {
+      return {
+        ...state,
+        user: action.updatedUser,
+        status: "resolved",
+        storedUser: null,
+        error: null,
+      };
+    }
+    case "fail update": {
+      return {
+        ...state,
+        status: "rejected",
+        error: action.error,
+        user: state.storedUser,
+        storedUser: null,
+      };
+    }
+    case "reset": {
+      return {
+        ...state,
+        status: null,
+        error: null,
+      };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
   }
-  function scrollToBottom() {
-    containerRef.current.scrollTop = containerRef.current.scrollHeight;
+}
+
+function UserProvider({ children }) {
+  const { user } = useAuth();
+  const [state, dispatch] = React.useReducer(userReducer, {
+    status: null,
+    error: null,
+    storedUser: user,
+    user,
+  });
+  const value = [state, dispatch];
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+function useUser() {
+  const context = React.useContext(UserContext);
+  if (context === undefined) {
+    throw new Error(`useUser must be used within a UserProvider`);
+  }
+  return context;
+}
+
+const updateUser = (dispatch, user, updates) => {
+  // 🐨 move the following logic to the `updateUser` function you create above
+  dispatch({ type: "start update", updates });
+  return userClient.updateUser(user, updates).then(
+    (updatedUser) => dispatch({ type: "finish update", updatedUser }),
+    (error) => dispatch({ type: "fail update", error })
+  );
+};
+
+function UserSettings() {
+  const [{ user, status, error }, userDispatch] = useUser();
+
+  const isPending = status === "pending";
+  const isRejected = status === "rejected";
+
+  const [formState, setFormState] = React.useState(user);
+
+  const isChanged = !dequal(user, formState);
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
   }
 
-  // 🐨 call useImperativeHandle here with your ref and a callback function
-  // that returns an object with scrollToTop and scrollToBottom
-  useImperativeHandle(ref, () => {
-    return { scrollToTop, scrollToBottom };
-  });
+  function handleSubmit(e) {
+    e.preventDefault();
+    updateUser(userDispatch, user, formState);
+  }
 
   return (
-    <div ref={containerRef} role="log">
-      {messages.map((message, index, array) => (
-        <div key={message.id}>
-          <strong>{message.author}</strong>: <span>{message.content}</span>
-          {array.length - 1 === index ? null : <hr />}
-        </div>
-      ))}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div style={{ marginBottom: 12 }}>
+        <label style={{ display: "block" }} htmlFor="username">
+          Username
+        </label>
+        <input
+          id="username"
+          name="username"
+          disabled
+          readOnly
+          value={formState.username}
+          style={{ width: "100%" }}
+        />
+      </div>
+      <div style={{ marginBottom: 12 }}>
+        <label style={{ display: "block" }} htmlFor="tagline">
+          Tagline
+        </label>
+        <input
+          id="tagline"
+          name="tagline"
+          value={formState.tagline}
+          onChange={handleChange}
+          style={{ width: "100%" }}
+        />
+      </div>
+      <div style={{ marginBottom: 12 }}>
+        <label style={{ display: "block" }} htmlFor="bio">
+          Biography
+        </label>
+        <textarea
+          id="bio"
+          name="bio"
+          value={formState.bio}
+          onChange={handleChange}
+          style={{ width: "100%" }}
+        />
+      </div>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setFormState(user);
+            userDispatch({ type: "reset" });
+          }}
+          disabled={!isChanged || isPending}
+        >
+          Reset
+        </button>
+        <button
+          type="submit"
+          disabled={(!isChanged && !isRejected) || isPending}
+        >
+          {isPending
+            ? "..."
+            : isRejected
+            ? "✖ Try again"
+            : isChanged
+            ? "Submit"
+            : "✔"}
+        </button>
+        {isRejected ? (
+          <pre style={{ color: "red" }}>{error.message}</pre>
+        ) : null}
+      </div>
+    </form>
   );
 }
 
-const MessagesDisplay = React.forwardRef(InnerComponent);
+function UserDataDisplay() {
+  const [{ user }] = useUser();
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
+}
 
 function App() {
-  const messageDisplayRef = React.useRef();
-  const [messages, setMessages] = React.useState(allMessages.slice(0, 8));
-  const addMessage = () =>
-    messages.length < allMessages.length
-      ? setMessages(allMessages.slice(0, messages.length + 1))
-      : null;
-  const removeMessage = () =>
-    messages.length > 0
-      ? setMessages(allMessages.slice(0, messages.length - 1))
-      : null;
-
-  const scrollToTop = () => messageDisplayRef.current.scrollToTop();
-  const scrollToBottom = () => messageDisplayRef.current.scrollToBottom();
-
   return (
-    <div className="messaging-app">
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <button onClick={addMessage}>add message</button>
-        <button onClick={removeMessage}>remove message</button>
-      </div>
-      <hr />
-      <div>
-        <button onClick={scrollToTop}>scroll to top</button>
-      </div>
-      <MessagesDisplay ref={messageDisplayRef} messages={messages} />
-      <div>
-        <button onClick={scrollToBottom}>scroll to bottom</button>
-      </div>
+    <div
+      style={{
+        minHeight: 350,
+        width: 300,
+        backgroundColor: "#ddd",
+        borderRadius: 4,
+        padding: 10,
+      }}
+    >
+      <UserProvider>
+        <UserSettings />
+        <UserDataDisplay />
+      </UserProvider>
     </div>
   );
 }
 
 export default App;
-
-const allMessages = [
-  `Leia: Aren't you a little short to be a stormtrooper?`,
-  `Luke: What? Oh... the uniform. I'm Luke Skywalker. I'm here to rescue you.`,
-  `Leia: You're who?`,
-  `Luke: I'm here to rescue you. I've got your R2 unit. I'm here with Ben Kenobi.`,
-  `Leia: Ben Kenobi is here! Where is he?`,
-  `Luke: Come on!`,
-  `Luke: Will you forget it? I already tried it. It's magnetically sealed!`,
-  `Leia: Put that thing away! You're going to get us all killed.`,
-  `Han: Absolutely, Your Worship. Look, I had everything under control until you led us down here. You know, it's not going to take them long to figure out what happened to us.`,
-  `Leia: It could be worse...`,
-  `Han: It's worse.`,
-  `Luke: There's something alive in here!`,
-  `Han: That's your imagination.`,
-  `Luke: Something just moves past my leg! Look! Did you see that?`,
-  `Han: What?`,
-  `Luke: Help!`,
-  `Han: Luke! Luke! Luke!`,
-  `Leia: Luke!`,
-  `Leia: Luke, Luke, grab a hold of this.`,
-  `Luke: Blast it, will you! My gun's jammed.`,
-  `Han: Where?`,
-  `Luke: Anywhere! Oh!!`,
-  `Han: Luke! Luke!`,
-  `Leia: Grab him!`,
-  `Leia: What happened?`,
-  `Luke: I don't know, it just let go of me and disappeared...`,
-  `Han: I've got a very bad feeling about this.`,
-  `Luke: The walls are moving!`,
-  `Leia: Don't just stand there. Try to brace it with something.`,
-  `Luke: Wait a minute!`,
-  `Luke: Threepio! Come in Threepio! Threepio! Where could he be?`,
-].map((m, i) => ({
-  id: i,
-  author: m.split(": ")[0],
-  content: m.split(": ")[1],
-}));
