51933832df4d3412b699dc80a70c747d11810d27
Soham Sengupta <soham.sengupta@axelerant.com>
2024-10-16 10:01:09 +0530

    Completed task

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import * as React from "react";
+import { Switch } from "./switch";
+
+function useToggle() {
+  const [on, setOn] = React.useState(false);
+  const toggle = () => setOn(!on);
+  const getTogglerProps = ({ onClick, ...props } = {}) => {
+    return {
+      "aria-pressed": "on",
+      onClick: () => {
+        onClick && onClick();
+        toggle();
+      },
+      ...props,
+    };
+  };
+  const togglerProps = {
+    "aria-pressed": "on",
+    onClick: toggle,
+  };
+  return { on, togglerProps, getTogglerProps };
+}
 
 function App() {
+  const { on, getTogglerProps } = useToggle();
   return (
-      <h1>My App</h1>
+    <div>
+      <Switch {...getTogglerProps({ on })} />
+      <hr />
+      <button
+        {...getTogglerProps({
+          "aria-label": "custom-button",
+          onClick: () => console.info("onButtonClick"),
+          id: "custom-button-id",
+        })}
+      >
+        {on ? "on" : "off"}
+      </button>
+    </div>
   );
 }
 
