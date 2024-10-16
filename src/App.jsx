2052b7e978ab14d4d8e8b6299d5fac1118f3a1e9
Soham Sengupta <soham.sengupta@axelerant.com>
2024-10-16 09:32:06 +0530

    Completed task

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,26 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
-        <ToggleButton />
+        <div>
+          <div>
+            <ToggleButton />
+          </div>
+        </div>
       </Toggle>
     </div>
   );
 }
 
 const Context = createContext();
+Context.displayName = "ToggleContext"; // For better dev tools readability.
+
+const useToggle = () => {
+  const data = useContext(Context);
+  if (!data) {
+    throw new Error("useToggle must be inside Toggle Context provider");
+  }
+  return data;
+};
 
 const Toggle = ({ children }) => {
   const [on, setOn] = React.useState(false);
@@ -23,15 +36,15 @@ const Toggle = ({ children }) => {
 };
 
 const ToggleOn = ({ children }) => {
-  const { on } = useContext(Context);
+  const { on } = useToggle();
   return on ? children : "";
 };
 const ToggleOff = ({ children }) => {
-  const { on } = useContext(Context);
+  const { on } = useToggle();
   return !on ? children : "";
 };
 const ToggleButton = () => {
-  const { on, setOn } = useContext(Context);
+  const { on, setOn } = useToggle();
   const toggle = () => setOn((p) => !p);
   return <Switch on={on} onClick={toggle} />;
 };
